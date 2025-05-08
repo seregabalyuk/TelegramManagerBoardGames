b5451989d01264725b193ec2badc64216e88de31
Serega Balyuk <seregabalyuk@gmail.com>
2025-05-09 01:22:10 +0800

    add readme

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,10 +1,13 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.enums import ParseMode
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 from database import User, Group
 
 router = Router()
+
+
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message):
@@ -16,7 +19,9 @@ async def cmd_start(message: types.Message):
   args = message.text.split()
   user = None
   registreted = False
+  answer = ""
 
+  # регистрация и добавление в группу
   try:
     user = User.get(id, username, registreted)
   except:
@@ -27,19 +32,13 @@ async def cmd_start(message: types.Message):
     У нас технические шоколадки
     База данных полетела(
     """
-    await message.answer(answer, parse_mode=ParseMode.MARKDOWN_V2)
   else:
     if registreted:
       answer = f"""
       Привет {first_name} {last_name}\\!
-      Я бот, который поможет отслеживать настольные игры твои и твоих друзей\\!
-      """
-      await message.answer(answer, parse_mode=ParseMode.MARKDOWN_V2)
+      Я бот, который поможет отслеживать настольные игры твои и твоих друзей\\!"""
     else:
-      answer = f"""
-      Привет {first_name} {last_name}\\!
-      """
-      await message.answer(answer, parse_mode=ParseMode.MARKDOWN_V2)
+      answer = f"""Привет {first_name} {last_name}\\!"""
     if len(args) > 1:
       param = args[1].split(sep='_')
       group_id = int(param[0])
@@ -48,20 +47,15 @@ async def cmd_start(message: types.Message):
       try:
         group = Group.load(group_id, password)
         if group.contain(user):
-          answer = f"""
-          Вы уже состоите в {group.title}\\!
-          """
+          answer += "\n" + f"Вы уже состоите в {group.title}\\!"
         else:
           group.add(user)
-          answer = f"""
-          Вы присоединились к {group.title}\\!
-          """
+          answer += "\n" + f"Вы присоединились к {group.title}\\!"
       except Exception as error:
         print ("cannot load group. with error:" + repr(error))
-        answer = f"""
-        Я не смог добавить вас в группу\\.
-        """
-      await message.answer(answer, parse_mode=ParseMode.MARKDOWN_V2)
+        answer += "\n" + f"Я не смог добавить вас в группу\\."
 
-        
+  await message.answer(answer, parse_mode=ParseMode.MARKDOWN_V2)
+  
+      
       
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
 import asyncio
 
 from handlers import start, groups
@@ -11,8 +12,19 @@ dp = Dispatcher()
 dp.include_router(start.router)
 dp.include_router(groups.router)
 
+
+async def set_bot_commands(bot: Bot):
+  commands = [
+    BotCommand(command="/start", description="Запуск бота"),
+    BotCommand(command="/add", description="Добавить настольную игру"),
+  ]
+  await bot.set_my_commands(commands)
+
+
 async def main():
+  await set_bot_commands(bot)
   await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
   asyncio.run(main())
